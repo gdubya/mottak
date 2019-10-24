9940faea09350f3806fc65aa461e3f4e00c6e63a
Per Buer <per.buer@gmail.com>
2019-10-24 22:40:50 +0200

    night

--- s3-scan/s3-avscan.py
+++ s3-scan/s3-avscan.py
@@ -1,5 +1,6 @@
 #!env python
 import os
+import sys
 import logging
 import pyclamd
 import boto3
@@ -10,9 +11,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def get_object(objectstore, bucket_name, object_name):
+def get_object(what, objectstore, bucket_name, object_name):
     """Retrieve an object from an Amazon S3 bucket
 
+    :param what: string of what to get: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.get_object
+    :param objectstore: boto3.client object
     :param bucket_name: string
     :param object_name: string
     :return: botocore.response.StreamingBody object. If error, return None.
@@ -22,12 +25,18 @@ def get_object(objectstore, bucket_name, object_name):
     except ClientError as e:
         logging.error(e)
         return None
-    return response['Body']
+    return response[what]
+
+
 
 
 def get_s3_handle():
+    """Retrieve an object from an Amazon S3 bucket
+
+    :return: boto3.client object. If error, return None.
+    """
+
     client_handle = None
-    print("S3 point for", os.getenv('AWS_ACCESS_KEY_ID'),":", os.getenv('AWS_SECRET_ACCESS_KEY'))
     
     try:
         client_handle = boto3.client('s3', 
@@ -36,16 +45,28 @@ def get_s3_handle():
                           aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
                           config=Config(signature_version='s3v4'),
                           region_name=os.getenv('REGION_NAME'))
-    except:
-        print("Could not create client handle")
-        raise
+    except Exception as e:
+        logging.error(e)
+        return None
     return client_handle
 
 
 
-def main():
+def scan():
     s3 = get_s3_handle()
-    file_stream = get_object(s3, os.getenv('BUCKET'), "test.txt")
+    if s3 is None:
+        logging.error("S3 client handle not defined")
+        raise Exception('S3 client handle not defined')
+    size = get_object('ContentLength', s3, os.getenv('BUCKET'), os.getenv('OBJECT'))
+
+    if (size > int(os.getenv('MAX_SCAN_SIZE'))):
+        print("Not scanning as object is bigger than MAX_SCAN_SIZE")
+        return None
+    print('Scanning', os.getenv('OBJECT'), 'from', os.getenv('BUCKET'), ' Size: ', size)
+    file_stream = get_object('Body', s3, os.getenv('BUCKET'), os.getenv('OBJECT'))
+    if file_stream is None: 
+        logging.error("Could not open file.")
+        raise Exception('Could not get S3 object handle')
     cd = pyclamd.ClamdUnixSocket(os.getenv('CLAMD_SOCK'))
     try:
         cd.ping()
@@ -53,10 +74,17 @@ def main():
         print("Failed to ping clamav deamon over socket:",os.getenv('CLAMD_SOCK') )
         raise
     print(cd.version())
-    print("Opening stream to ClamAV")
-    #dump_stream(file_stream)
-    print(cd.scan_stream(file_stream))
 
+    result = cd.scan_stream(file_stream)
+    if result is None:
+        print("No viruses found")
+        return None
+    else:
+        print("Virus found: ", result)
+        return result
 
 if __name__ == '__main__':
-    main()
+    if scan():
+        sys.exit(1)
+    else:
+        sys.exit(0)
