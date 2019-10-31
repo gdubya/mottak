4eb9c91ebdb4170735c7c63fd2a03f48541de516
Per Buer <per.buer@gmail.com>
2019-10-31 09:35:09 +0100

    Added watching + rename env to .env

--- argo/submit.sh
+++ argo/submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo Submitting Argo workflow $1 with parameters from env file
-argo submit $(for line in $(cat env);do echo "--parameter $line";done) $1
+argo submit --watch $(for line in $(cat .env);do echo "--parameter $line";done) $1
 
