3c9afa50c7e6f09c5500da74019bfd1b3a0101b5
Per Buer <per.buer@gmail.com>
2019-11-06 13:35:33 +0100

    Cleanup. Stop using arkade.sh, add some comments to document

--- arkade5/start.sh
+++ arkade5/start.sh
@@ -2,22 +2,30 @@
 
 echo Fetching via s3
 
+echo Using $AWS_ACCESS_KEY_ID fetch via s3
+echo Endpoint: $ENDPOINT
+echo Object: $OBJECT
+
 
 # transform endpoint (URL) to hostname
+# Note that S3 will break if there is a trailing slash.
+# Todo: create a sed expressions that cleans better.
 host=$(echo $ENDPOINT|sed 's,https://,,g')
 basename=$(basename -s .tar $OBJECT)
 
 cd /opt
 mkdir input output tmp
 cd input
-s3cmd get --access_key $AWS_ACCESS_KEY_ID --secret_key $AWS_SECRET_ACCESS_KEY --host-bucket $host --host $host s3://$BUCKET/$OBJECT
+# Add --debug after s3cmd to debug S3-related issues.
+s3cmd --access_key $AWS_ACCESS_KEY_ID --secret_key $AWS_SECRET_ACCESS_KEY --host-bucket $host --host $host s3://$BUCKET/$OBJECT
 
 # Hent ut METS-fil fra tarballen:
 tar xf $OBJECT "$basename/dias-mets.xml"
 
-# Arkade får spader om vi ikke er i samme katalog.
-cd /opt/Arkade5CLI-1.5.0
 
+# kjør arkade direkte 
+dotnet /opt/Arkade5CLI-1.5.0/Arkivverket.Arkade.CLI.dll -a /opt/input/$OBJECT -p /opt/tmp -o /opt/output -s packing -m /opt/input/$basename/dias-mets.xml -t noark5
 
-
-./arkade.sh -a /opt/input/$OBJECT -p /opt/tmp -o /opt/output -s packing -m /opt/input/$basename/dias-mets.xml -t noark5
+# The report is available at /opt/output/Arkaderapport-$basename.html
+# Move it to a know location so Argo can get at it.
+mv /opt/output/Arkaderapport-$basename.html /tmp/arkade.html
