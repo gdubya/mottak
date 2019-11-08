886e8c824cbe28c0de3497875ab32d2021345611
Per Buer <per.buer@gmail.com>
2019-11-08 13:58:22 +0100

    Extract artifact + remove debug

--- arkade5/start.sh
+++ arkade5/start.sh
@@ -5,6 +5,7 @@ echo Fetching via s3
 echo Using $AWS_ACCESS_KEY_ID fetch via s3
 echo Endpoint: $ENDPOINT
 echo Object: $OBJECT
+echo Archieve type: $ARCHIEVE_TYPE
 
 
 # transform endpoint (URL) to hostname
@@ -13,18 +14,24 @@ echo Object: $OBJECT
 host=$(echo $ENDPOINT|sed 's,https://,,g')
 basename=$(basename -s .tar $OBJECT)
 
+mkdir -p /opt
 cd /opt
 mkdir input output tmp
 cd input
 # Add --debug after s3cmd to debug S3-related issues.
-s3cmd --access_key $AWS_ACCESS_KEY_ID --secret_key $AWS_SECRET_ACCESS_KEY --host-bucket $host --host $host s3://$BUCKET/$OBJECT
+s3cmd get \
+     --access_key $AWS_ACCESS_KEY_ID \
+     --secret_key $AWS_SECRET_ACCESS_KEY \
+     --host-bucket $host --host $host s3://$BUCKET/$OBJECT
 
 # Hent ut METS-fil fra tarballen:
 tar xf $OBJECT "$basename/dias-mets.xml"
 
 
 # kjør arkade direkte 
-dotnet /opt/Arkade5CLI-1.5.0/Arkivverket.Arkade.CLI.dll -a /opt/input/$OBJECT -p /opt/tmp -o /opt/output -s packing -m /opt/input/$basename/dias-mets.xml -t noark5
+dotnet /opt/Arkade5CLI-1.5.0/Arkivverket.Arkade.CLI.dll -a \
+    /opt/input/$OBJECT -p /opt/tmp -o /opt/output -s packing \
+    -m /opt/input/$basename/dias-mets.xml -t $ARCHIEVE_TYPE
 
 # The report is available at /opt/output/Arkaderapport-$basename.html
 # Move it to a know location so Argo can get at it.
