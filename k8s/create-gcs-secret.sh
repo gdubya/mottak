c82acc0b7f2fc016715874f2f368a2cd1f28e484
Per Buer <per.buer@gmail.com>
2019-11-19 19:35:22 +0100

    move secrets away. inject checksum into argo

--- k8s/create-gcs-secret.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo Looking for gcs.json. Will create the k8s secret gcs-cred which will contain the file
-echo in the container it will be available according to the deployment.
-
-kubectl create secret generic gcs-cred  --from-file=gcs.json
--- k8s/create-s3-secrets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# secrets are stored in two files
-# accessKeySecret - the AWS key
-# secretKeySecret - the AWS secret key
-
-kubectl create secret generic s3-cred  --from-file=accessKeySecret=./accessKeySecret --from-file=secretKeySecret=./secretKeySecret
-
