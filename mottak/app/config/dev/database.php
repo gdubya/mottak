33893121f02f220ed0bccf5f4ff73e274a88fe87
Frederic G. Østby <frederic.g.ostby@gmail.com>
2019-11-20 12:40:36 +0100

    Updated configs

--- mottak/app/config/dev/database.php
+++ mottak/app/config/dev/database.php
@@ -2,38 +2,15 @@
 
 return
 [
-	/*
-	 * ---------------------------------------------------------
-	 * Default
-	 * ---------------------------------------------------------
-	 *
-	 * Default configuration to use.
-	 */
-	'default' => 'mottak',
+	'default' => 'cloudsql',
 
-	/*
-	 * ---------------------------------------------------------
-	 * Configurations
-	 * ---------------------------------------------------------
-	 *
-	 * You can define as many database configurations as you want.
-	 *
-	 * dsn        : PDO data source name
-	 * username   : (optional) Username of the database server
-	 * password   : (optional) Password of the database server
-	 * persistent : (optional) Set to true to make the connection persistent
-	 * log_queries: (optional) Enable query logging?
-	 * reconnect  : (optional) Should the connection automatically be reestablished?
-	 * options    : (optional) An array of PDO options
-	 * queries    : (optional) Queries that will be executed right after a connection has been made
-	 */
 	'configurations' =>
 	[
-		'mottak' =>
+		'cloudsql' =>
 		[
-			'dsn'         => 'pgsql:dbname=mottak;host=10.17.23.10;port=5432;options=\'--client_encoding=UTF-8\'',
-			'username'    => 'postgres',
-			'password'    => 'cantina',
+			'dsn'         => 'pgsql:dbname=mottak;host=' . getenv('DB_HOST') . ';port=5432;options=\'--client_encoding=UTF-8\'',
+			'username'    => getenv('DB_USER'),
+			'password'    => getenv('DB_PASSWORD'),
 			'persistent'  => false,
 			'log_queries' => true,
 		],
