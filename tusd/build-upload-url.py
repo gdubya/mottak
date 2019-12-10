a59f6592101bf191e7f2001a18dbf5fd4b269fda
Per Buer <per.buer@gmail.com>
2019-12-10 15:49:20 +0100

    add shebang, make python 3 compliant

--- tusd/build-upload-url.py
+++ tusd/build-upload-url.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env python
+
 import argparse
 import base64
 import json
 import uuid
 
-parser = argparse.ArgumentParser(description='Build a Uploader URL for testing.')
+parser = argparse.ArgumentParser(
+    description='Build a Uploader URL for testing.')
 
-parser.add_argument('-u', '--upload-url', type=str, required=True, help='The tusd endpoint URL')
-parser.add_argument('-i', '--invitation-id', type=int, required=True, help='The invitation id')
+parser.add_argument('-u', '--upload-url', type=str,
+                    required=True, help='The tusd endpoint URL')
+parser.add_argument('-i', '--invitation-id', type=int,
+                    required=True, help='The invitation id')
 
 args = parser.parse_args()
 
@@ -17,6 +22,11 @@ url = {
     'meta': {
         'invitation_id': args.invitation_id,
     },
-}
+    'Storage': {
+        'key': args.storage_key,
+    },
 
-print('dpldr://%s' % (base64.b64encode(json.dumps(url))))
+}
+# The decode bullshit is there because we wanna avoid the b' marker at the beginning of binary strings.
+b64json = base64.b64encode(json.dumps(url).encode()).decode('utf-8')
+print(f'dpldr://{b64json}')
