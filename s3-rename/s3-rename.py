0a3faa8f867ea1aaddd844c573a5ab12753a749c
Per Buer <per.buer@gmail.com>
2019-12-15 09:03:59 +0100

    use the resource, not client

--- s3-rename/s3-rename.py
+++ s3-rename/s3-rename.py
@@ -10,25 +10,22 @@ load_dotenv()
 ENVERROR = 1
 RENAMEERROR = 2
 
-
 def rename():
-
-    file = os.getenv('OBJECT')
+    filename = os.getenv('OBJECT')
     newname = os.getenv('NEWNAME')
     bucket = os.getenv('BUCKET')
-
-    s3 = ar.get_s3_handle()
+    s3 = ar.get_s3_resource()
     if s3 is None:
         logging.error("S3 client handle not defined")
         raise Exception('S3 client handle not defined')
-    if ar.rename_object(objectstore=s3, bucket_name=bucket,
-                        object_name=file, newname=newname):
-        logging.info(f"Renamed {file} succesfully from {bucket} to {newname}")
+    print(f'Copying {filename} to {newname} in {bucket}')
+    if ar.bucket_copy_object(objectstore=s3, bucket_name=bucket,
+                             object_name=filename, target=newname):
+        logging.info(f"Renamed {filename} succesfully from {bucket} to {newname}")
         return(True)
     else:
-        logging.error(f"Failed to rename {file} from {bucket} to {newname}")
+        logging.error(f"Failed to rename {filename} from {bucket} to {newname}")
         return(False)
-
 
 if ar.verify_environment(['ENDPOINT', 'AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY',
                           'REGION_NAME', 'BUCKET', 'OBJECT', 'NEWNAME']):
