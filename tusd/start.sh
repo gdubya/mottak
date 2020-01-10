36f823c23ce56d52fddbb60301001bd26ce5e39b
Per Buer <per.buer@gmail.com>
2020-01-10 10:12:38 +0100

    switch from GCS to AWS S3

--- tusd/start.sh
+++ tusd/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-echo tusd command line: tusd --hooks-dir /srv/tusd-hooks --behind-proxy --gcs-bucket $BUCKET
-tusd --hooks-dir /srv/tusd-hooks --behind-proxy --gcs-bucket $BUCKET
+# clean up endpoint
+MY_ENDPOINT=$(echo $ENDPOINT|sed 's,https://,,g')
+
+TUSD_PARAMS="--hooks-dir /sr/tusd-hooks --behind-proxy --s3-bucket $BUCKET --s3-endpoint $MY_ENDPOINT"
+echo tusd command line: tusd $TUSD_PARAMS
+tusd $TUSD_PARAMS
 
