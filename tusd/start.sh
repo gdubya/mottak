1fcf533fce71554cb4561b9e5b66068be5f2d821
Per Buer <per.buer@gmail.com>
2020-01-24 15:49:02 +0100

    handle alt. base path for tusd

--- tusd/start.sh
+++ tusd/start.sh
@@ -2,7 +2,13 @@
 # clean up endpoint
 MY_ENDPOINT=$(echo $ENDPOINT|sed 's,https://,,g')
 echo "Endpoint transformation: $ENDPOINT --> $MY_ENDPOINT"
+OPT_PARAMS=""
 
+# Handle optional configuration from environment:
+if [ -n "$BASE_PATH" ]; then
+    OPT_PARAMS="$OPT_PARAMS --base-path $BASE_PATH"
+fi
+# Pick between GCS and S3
 if [ -n "$GCS_SERVICE_ACCOUNT_FILE" ]; then
     echo "Assuming we're using the GCS native backend."
     if [ ! -a $GCS_SERVICE_ACCOUNT_FILE ]; then
@@ -14,6 +20,6 @@ else
     TUSD_PARAMS="--hooks-dir /srv/tusd-hooks --behind-proxy --s3-bucket $BUCKET --s3-endpoint $MY_ENDPOINT"
 fi
 
-echo tusd command line: tusd $TUSD_PARAMS
-tusd $TUSD_PARAMS
+echo tusd command line: "tusd $TUSD_PARAMS $OPT_PARAMS"
+tusd $TUSD_PARAMS $OPT_PARAMS
 
