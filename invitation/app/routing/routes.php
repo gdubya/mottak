7d232d70189dd872102b59173b5fb6e0bf718df0
Per Buer <per.buer@gmail.com>
2020-01-30 09:26:23 +0100

    Revert routing

--- invitation/app/routing/routes.php
+++ invitation/app/routing/routes.php
@@ -6,18 +6,15 @@ use mako\http\routing\Routes;
 
 $routes->group(['namespace' => 'app\http\controllers', 'middleware' => ['security_headers', 'input_validation']], function(Routes $routes): void
 {
-	# Todo: This should be replaced with a simpler service that checks DB connectivity. It is only used by the heartbeat function in the
-	# service and ingress controllers.
+
 	$routes->get('/', 'Invitations::start', 'invitations.start');
 
-	$routes->get('/invitation/', 'Invitations::start', 'invitations.start');
 
+	$routes->post('/', 'Invitations::parseXml');
 
-	$routes->post('/invitation/', 'Invitations::parseXml');
+	$routes->get('/create', 'Invitations::create', 'invitations.create');
 
-	$routes->get('/invitation/create', 'Invitations::create', 'invitations.create');
+	$routes->post('/create', 'Invitations::store');
 
-	$routes->post('/invitation/create', 'Invitations::store');
-
-	$routes->get('/invitation/receipt/{id}', 'Invitations::receipt', 'invitations.receipt')->patterns(['id' => '[0-9]+']);
+	$routes->get('/receipt/{id}', 'Invitations::receipt', 'invitations.receipt')->patterns(['id' => '[0-9]+']);
 });
