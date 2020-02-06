3ef39ebe34e707e80673eb2e9e71097fdecb5fb9
Per Buer <per.buer@gmail.com>
2020-02-06 10:29:43 +0100

    switch API

--- s3-delete/s3-delete.py
+++ s3-delete/s3-delete.py
@@ -2,7 +2,7 @@
 import os
 import sys
 import logging
-import ar_s3_helper as ar
+from av_objectstore import ArkivverketObjectStorage
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -10,33 +10,13 @@ load_dotenv()
 ENVERROR = 1
 DELETEERROR = 2
 
-def delete():
-    filename = os.getenv('OBJECT')
-    bucket = os.getenv('BUCKET')
+bucket = os.getenv('BUCKET')
+filename = os.getenv('OBJECT')
 
-    s3 = ar.get_s3_resource()
-    if s3 is None:
-        logging.error("S3 client handle not defined")
-        raise Exception('S3 client handle not defined')
-
-    obj = s3.Object(bucket, filename)
-
-    if obj.delete():
-        logging.info(f"Object deleted {filename} succesfully from {bucket}")
-        return(True)
-    else:
-        logging.error(f"Failed to delete {filename} from {bucket}")
-        return(False)
-
-
-if ar.verify_environment(['ENDPOINT', 'AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'REGION_NAME', 'BUCKET', 'OBJECT']):
-    logging.info("Environment verified OK")
+storage = ArkivverketObjectStorage()
+if storage.delete(bucket, filename):
+    logging.info(f"Object deleted {filename} succesfully from {bucket}")
+    sys.exit(0)
 else:
-    logging.error("Enviroment not set properly")
-    exit(ENVERROR)
-
-if __name__ == '__main__':
-    if delete():
-        sys.exit(0)
-    else:
-        sys.exit(DELETEERROR)
+    logging.error(f"Failed to delete {filename} from {bucket}")
+    sys.exit(DELETEERROR)
