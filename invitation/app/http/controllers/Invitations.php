41b416ca77e44941768a811d640aed86c36f97bf
Frederic G. Østby <frederic.g.ostby@gmail.com>
2020-02-20 20:47:17 +0100

    Update Invitations.php

--- invitation/app/http/controllers/Invitations.php
+++ invitation/app/http/controllers/Invitations.php
@@ -217,7 +217,7 @@ class Invitations extends Controller
 		]);
 
 		$invitation->archive_type_id = $input['archive_type_id'];
-		$invitation->is_sensitive    = $input['is_sensitive'] == = '1' ? true : false;
+		$invitation->is_sensitive    = $input['is_sensitive'] === '1' ? true : false;
 		$invitation->name            = $input['name'];
 		$invitation->email           = $input['email'];
 
