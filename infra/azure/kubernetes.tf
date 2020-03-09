6a4aec07a77d03a598c6097f5742e4bd3bd8cd84
Martin Setek <marset@arkivverket.no>
2020-03-09 16:36:25 +0100

    do not output cert

--- infra/azure/kubernetes.tf
+++ infra/azure/kubernetes.tf
@@ -25,10 +25,6 @@ resource "azurerm_kubernetes_cluster" "mottak-cluster" {
   }
 }
 
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.mottak-cluster.kube_config.0.client_certificate
-}
-
 output "kube_config" {
    value = azurerm_kubernetes_cluster.mottak-cluster.kube_config_raw
 }
